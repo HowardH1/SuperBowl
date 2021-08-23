3f3fd3ec4b287a25e92809b0746a57604916b058
Howard Hall <56894894+HowardH1@users.noreply.github.com>
2021-08-23 14:10:44 -0400

    Began work on bridge elevation

---
+++ SuperBowl/Assets/BridgeLift.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeLift : MonoBehaviour
+{
+    public Collider target;
+    public testController player;
+    public Rigidbody rb;
+    public GameObject bridge;
+    public bool triggered = false;
+    private Vector3 bridgeBuffer;
+
+    private void Start()
+    {
+        bridgeBuffer = bridge.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision != target)
+        {
+            return;
+        }
+        else
+        {
+            bridge.transform.position = Vector3.Lerp(bridgeBuffer, new Vector3(0, bridge.transform.position.y - 15, 0), 3f );
+            triggered = true;
+        }
+    }
+}
