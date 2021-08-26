54fc386e2f1742894cae1ec03848f42932537f0d
HowardH1 <56894894+HowardH1@users.noreply.github.com>
2021-08-26 18:58:29 -0400

    Continued work on BridgeLift Script

--- SuperBowl/Assets/Scripts/BridgeLift.cs
+++ SuperBowl/Assets/Scripts/BridgeLift.cs
@@ -8,10 +8,11 @@ public class BridgeLift : MonoBehaviour
     public testController player;
     public Rigidbody rb;
     public GameObject bridge;
-    public int height;
-    public int speed;
+    public int heightLimit;
     public bool triggered = false;
-    private Vector3 bridgeBuffer;
+    public bool returnToOrigin = false;
+    public int height;
+    public Vector3 bridgeBuffer;
 
     private void Start()
     {
@@ -20,11 +21,7 @@ public class BridgeLift : MonoBehaviour
 
     private void Update()
     {
-        if(triggered == true)
-        {
-            Vector3 move = new Vector3(0, 0, height);
-            bridge.transform.Translate(move);
-        }
+        checkBridgeMovement();
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -36,6 +33,32 @@ public class BridgeLift : MonoBehaviour
         else
         {
             triggered = true;
+        }
+    }
+
+    private void checkBridgeMovement()
+    {
+        if (triggered == true)
+        {
+            Debug.Log("IF 0");
+            if (bridge.transform.position.y !>= heightLimit && returnToOrigin == false)
+            {
+                bridge.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+                Debug.Log("IF 1");
+            }
+            else if (bridge.transform.position.y >= heightLimit)
+            {
+                returnToOrigin = true;
+                bridge.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+                Debug.Log("IF 2");
+            }
+            else if (bridge.transform.position.y <= bridgeBuffer.y && returnToOrigin == true)
+            {
+                bridge.transform.position = bridgeBuffer;
+                returnToOrigin = false;
+                triggered = false;
+                Debug.Log("IF 3");
+            }
         }
     }
 }
