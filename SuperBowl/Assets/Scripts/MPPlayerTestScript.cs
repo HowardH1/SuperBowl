dbc74b001b3ca6c0770721bef5c76c58d3f200a7
HowardH1 <56894894+HowardH1@users.noreply.github.com>
2021-09-13 22:47:03 -0400

    Added Movement for Networked Players, Unique Coloring,

---
+++ SuperBowl/Assets/Scripts/MPPlayerTestScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class MPPlayerTestScript : NetworkBehaviour
+{
+    public float movementSpeed = 5f;
+    public Transform camT;
+    CharacterController mpCharController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mpCharController = GetComponent<CharacterController>();
+        if(IsLocalPlayer)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.black;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        if(!IsLocalPlayer)
+        {
+            camT.GetComponent<Camera>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsLocalPlayer)
+        {
+            MPMovePlayer();
+        }
+    }
+
+    void MPMovePlayer()
+    {
+        Vector3 moveVect = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        mpCharController.SimpleMove(moveVect * movementSpeed);
+    }
+}
