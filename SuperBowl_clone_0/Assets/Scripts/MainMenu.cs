313c59b613179e3fc8fb1e15810c45b34c2937c0
HowardH1 <56894894+HowardH1@users.noreply.github.com>
2021-09-27 23:47:33 -0400

    Added Network Demo Menu, Added Projectile Network Mapping

--- SuperBowl_clone_0/Assets/Scripts/MainMenu.cs
+++ SuperBowl_clone_0/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,17 @@ public class MainMenu : MonoBehaviour
 {
     public void playGame() 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void hostGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void joinGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void quitGame()
